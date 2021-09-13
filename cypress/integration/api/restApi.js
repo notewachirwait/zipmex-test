7d6462c91dccd8f4f78cbb840eeda697f0a7094c
note wachirawit <note.wachirawit@maqe.com>
2021-09-13 23:21:28 +0700

    add test case api

---
+++ cypress/integration/api/restApi.js
@@ -0,0 +1,20 @@
+import {markdownTable} from 'markdown-table';
+
+describe('Create markdown table', () => {
+  it('Must return a markdown table format correctly', () => {
+    cy.request('GET', 'https://public-api.zipmex.net/api/v1.0/summary').then((resp) => {
+    const rows = [];
+    const marketData = resp.body.data;
+    const columns = ['instrument', 'last_price', 'lowest_24hr', 'highest_24hr'];
+    Object.entries(marketData).forEach(([key, value]) => {
+      rows.push([key,
+        value.last_price,
+        value.lowest_24hr,
+        value.highest_24hr
+      ]);
+    });
+    rows.unshift(columns);
+    cy.writeFile('cypress/markdown/log.txt', markdownTable(rows)); 
+    });
+  });
+});
