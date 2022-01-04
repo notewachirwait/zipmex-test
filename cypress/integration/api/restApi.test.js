914a96a26328faca81f74b9380e1bd9220027ce9
Note <wachirawit.th@ku.th>
2022-01-04 14:41:44 +0700

    commit

--- cypress/integration/api/restApi.test.js
+++ cypress/integration/api/restApi.test.js
@@ -1,22 +1,33 @@
-import {markdownTable} from 'markdown-table';
+import { markdownTable } from "markdown-table";
 
-describe('Create markdown table', () => {
-  it('Must return a markdown table format correctly', () => {
-    cy.request('GET', 'https://public-api.zipmex.net/api/v1.0/summary').then((resp) => {
-    const rows = [];
-    const marketData = resp.body.data;
-    const columns = ['instrument', 'last_price', 'lowest_24hr', 'highest_24hr'];
-    Object.entries(marketData).forEach(([key, value]) => {
-      rows.push([key,
-        value.last_price,
-        value.lowest_24hr,
-        value.highest_24hr
-      ]);
-    });
-    
-    rows.unshift(columns);
-    cy.writeFile('cypress/log/result.txt', 'Zipmex market cap\n'); 
-    cy.writeFile('cypress/log/result.txt', markdownTable(rows), { flag: 'a+' }); 
-    });
+describe("Create markdown table", () => {
+  it("Must return a markdown table format correctly", () => {
+    cy.log(Cypress.env("TEST"));
+    cy.request("GET", "https://public-api.zipmex.net/api/v1.0/summary").then(
+      (resp) => {
+        const rows = [];
+        const marketData = resp.body.data;
+        const columns = [
+          "instrument",
+          "last_price",
+          "lowest_24hr",
+          "highest_24hr",
+        ];
+        Object.entries(marketData).forEach(([key, value]) => {
+          rows.push([
+            key,
+            value.last_price,
+            value.lowest_24hr,
+            value.highest_24hr,
+          ]);
+        });
+
+        rows.unshift(columns);
+        cy.writeFile("cypress/log/result.txt", "Zipmex market cap\n");
+        cy.writeFile("cypress/log/result.txt", markdownTable(rows), {
+          flag: "a+",
+        });
+      }
+    );
   });
 });
